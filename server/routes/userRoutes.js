e1f8d4e7b716f580d5ababbf36b638c14de31969
BulzEye <shirgaonkarom@yahoo.in>
2023-01-17 03:53:23 +0530

    updated admin list

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 // userName
 // userType
 
-const adminEmails = ['ee200002040@iiti.ac.in', 'ce200004001@iiti.ac.in']
+const adminEmails = ['ee200002040@iiti.ac.in', 'ce200004001@iiti.ac.in', 'cse200001057@iiti.ac.in', 'ee200002001@iiti.ac.in']
 
 const generateJWTToken = (userId, userName, userEmail, userType) => {
     return jwt.sign({ userId, userName, userEmail, userType }, process.env.JWT_SECRET_KEY);
