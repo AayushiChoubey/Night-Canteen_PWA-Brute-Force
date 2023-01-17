eebc4613528d5b74841d514568559ec4bbdd9008
BulzEye <shirgaonkarom@yahoo.in>
2023-01-17 20:56:26 +0530

    add live date and time to admin dashboard

--- client/src/admin/pages/AdminDashboard/AdminDashboard.js
+++ client/src/admin/pages/AdminDashboard/AdminDashboard.js
@@ -2,9 +2,27 @@
 import { useSelector } from 'react-redux';
 import AdminDashboardOrderCard from '../../components/AdminDashboardOrderCard/AdminDashboardOrderCard';
 import { Card } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
 
 const AdminDashboard = () => {
     const orders = useSelector((state) => state.orders ? state.orders.value : null);
+
+    const [curDate, setCurDate] = useState(new Date());
+
+    // Function that displays the date in the dashboard. Modify to change format of date
+    const displayDate = () => {
+        return curDate.toString().substring(0, curDate.toString().lastIndexOf(":"));
+    };
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurDate(new Date())
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        }
+    });
 
     return (
         <div>
@@ -24,8 +42,10 @@ const AdminDashboard = () => {
                 backgroundColor: "#FFC107"
             }}>
                 <Card.Header>
-                    {/* ToDo: Add live Date and Time */}
-                    <h3 className="mb-0 text-center text-dark">Sunday, 15 Jan 11:43 PM</h3>
+                    <h3 className="mb-0 text-center text-dark">
+                        {/* Sunday, 15 Jan 11:43 PM */}
+                        {displayDate()}
+                    </h3>
                 </Card.Header>
                 <Card.Body>
                     <h5>Total Orders : {orders ? orders.length : `---`}</h5>
